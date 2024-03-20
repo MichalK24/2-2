c0669d47ae2f35ba90d93ce34affe4cb887a0d38
MichalK24 <162459522+MichalK24@users.noreply.github.com>
2024-03-20 14:04:48 +0000

    2-1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,24 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;2323sc
+// import static org.junit.jupiter.api.Assertions.assertEquals;232cgyhde
 
 // import org.junit.jupiter.api.Test;
-public  class Main {
-    public static void main(String[] args) {
-        String text = "HELLO WORLD";
-        for (int i = 0; i < text.length(); i++) {
-            System.out.println(text.charAt(i));
-        }
-    }
-}
+import java.util.Scanner;
+
+public class Main
+{
+  public static void main(String[] args)
+  { //Nie rozumiem czemu nie  dziala
+      Scanner scanner = new Scanner(System.in);
+       System.out.print("PODAJ 5 Liczb:")
+      int NajmniejszaLiczba = scanner.nextInt();
+         for (int  i = 0; i < 5; i++) 
+    {
+    System.out.print("Podaj liczbę " + (i + 1) + ": ");
+    int Liczba = scanner.nextInt();
+      if (Liczba < NajmniejszaLiczba) 
+      {   
+              NajmniejszaLiczba= Liczba;
+          }
+      }
+    System.out.println("najmniejsza liczba");
+  }  
+} 
